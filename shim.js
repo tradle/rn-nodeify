62f95afcac81fef9dd7ff4387a8f0287b4943a28
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-09-28 17:07:04 +0100

    augment 'process' module if it exists, instead of skipping

--- shim.js
+++ shim.js
@@ -2,8 +2,16 @@ if (typeof __dirname === 'undefined') global.__dirname = '/'
 if (typeof __filename === 'undefined') global.__filename = ''
 if (typeof process === 'undefined') {
   global.process = require('process')
-  process.browser = false
+} else {
+  var bProcess = require('process')
+  for (var p in bProcess) {
+    if (!(p in process)) {
+      process[p] = bProcess[p]
+    }
+  }
 }
+
+process.browser = false
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
 // global.location = global.location || { port: 80 }
