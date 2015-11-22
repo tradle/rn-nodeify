bd9d7f9f0f8b1f31a8e126d370006fe476bb58c7
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-11-22 12:37:59 +0000

    use __DEV__ to set NODE_ENV and localStorage.debug

--- shim.js
+++ shim.js
@@ -15,3 +15,8 @@ process.browser = false
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
 // global.location = global.location || { port: 80 }
+var isDev = typeof __DEV__ === 'boolean' && __DEV__
+process.env.NODE_ENV = isDev ? 'development' : 'production'
+if (typeof localStorage !== 'undefined') {
+  localStorage.debug = isDev ? '*' : ''
+}
