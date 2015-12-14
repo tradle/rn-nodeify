tradle/rn-nodeify

commit 4c6b199a153626bf1394930d3de7764386a5cfbb

separate install from pkg-hacks

cmd.js
@@ -18,6 +18,7 @@ var pkg = require(pkgPath)
 var hackFiles = require('./pkg-hacks')
 var argv = minimist(process.argv.slice(2), {
   alias: {
+    i: 'install',
     e: 'extra'
   }
 })
@@ -26,40 +27,38 @@ run()
 
 function run () {
   var toShim
-  if (argv._.length) {
-    toShim = argv._
-    // if (toShim.indexOf('stream') !== -1) {
-    // }
+  if (argv.install) {
+    if (argv.install === 'all') {
+      toShim = coreList
+    } else {
+      toShim = argv.install.split(',')
+      if (toShim.indexOf('stream') !== -1) {
+        toShim.push(
+          '_stream_transform',
+          '_stream_readable',
+          '_stream_writable',
+          '_stream_duplex',
+          '_stream_passthrough',
+          'readable-stream'
+        )
+      }
+    }
 
-    // var browserB = {}
-    // toShim.forEach(function (m) {
-    //   if (allShims[m]) {
-    //     browserB[m] = allShims[m]
-    //   }
-    // })
+    if (toShim.indexOf('crypto') !== -1) {
+      toShim.push('react-native-randombytes')
+    }
 
-    // browser = browserB
+    installShims(toShim, function (err) {
+      if (err) throw err
+
+      runHacks()
+    })
   } else {
     toShim = coreList
+    runHacks()
   }
 
-  toShim = toShim.slice()
-  toShim.push(
-    '_stream_transform',
-    '_stream_readable',
-    '_stream_writable',
-    '_stream_duplex',
-    '_stream_passthrough',
-    'readable-stream'
-  )
-
-  if (toShim.indexOf('crypto') !== -1) {
-    toShim.push('react-native-randombytes')
-  }
-
-  installShims(toShim, function (err) {
-    if (err) throw err
-
+  function runHacks () {
     hackPackageJSONs(toShim, function (err) {
       if (err) throw err
 
@@ -68,7 +67,7 @@ function run () {
         else hackFiles([].concat(argv.hack))
       }
     })
-  })
+  }
 }
 
 function installShims (modulesToShim, done) {

commit bac7668c00666cb9704b8fd871633d938487d6f8

fix --hack broken by 4c6b199a

cmd.js
@@ -38,29 +38,36 @@ function run () {
       toShim = coreList
     } else {
       toShim = argv.install.split(',')
-      if (toShim.indexOf('stream') !== -1) {
-        toShim.push(
-          '_stream_transform',
-          '_stream_readable',
-          '_stream_writable',
-          '_stream_duplex',
-          '_stream_passthrough',
-          'readable-stream'
-        )
-      }
+        .map(function (name) {
+          return name.trim()
+        })
     }
+  } else {
+    toShim = coreList
+  }
 
-    if (toShim.indexOf('crypto') !== -1) {
-      toShim.push('react-native-randombytes')
-    }
+  if (toShim.indexOf('stream') !== -1) {
+    toShim.push(
+      '_stream_transform',
+      '_stream_readable',
+      '_stream_writable',
+      '_stream_duplex',
+      '_stream_passthrough',
+      'readable-stream'
+    )
+  }
 
+  if (toShim.indexOf('crypto') !== -1) {
+    toShim.push('react-native-randombytes')
+  }
+
+  if (argv.install) {
     installShims(toShim, function (err) {
       if (err) throw err
 
       runHacks()
     })
   } else {
-    toShim = coreList
     runHacks()
   }
 